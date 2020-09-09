b0d0fa8fe9e7850a5923e1446daa1213ddcd5b58
Aftab 'SHUR1KEN' Alam <42811989+SHUR1K-N@users.noreply.github.com>
2020-09-09 16:14:10 +0530

    Added headless mode [MacroNinja (Headless).pyw]

---
+++ MacroNinja (Headless).py
@@ -0,0 +1,56 @@
+import keyboard; import time
+import re; import os
+
+
+macrosSoft = {}
+macrosHard = {}
+
+softModifier = "ctrl"
+hardModifier = "ctrl + alt"
+
+
+def macroMatch(self):
+    for entryKey in chosenDict:
+        if self.name == entryKey:
+            keyboard.write(chosenDict[entryKey])
+    keyboard.unhook_all()
+
+
+######################### Main #########################
+
+if __name__ == "__main__":
+
+    with open("Soft Macros.cfg", "r") as file:
+        tempString = file.read()
+        tempListSoft = re.findall(r"~+[0-9]+\.\s(.+)", tempString)
+        for index, element in enumerate(tempListSoft):
+            if tempListSoft[index] == "empty":
+                tempListSoft[index] = ""
+
+    with open("Hard Macros.cfg", "r") as file:
+        tempString = file.read()
+        tempListHard = re.findall(r"~+[0-9]+\.\s(.+)", tempString)
+        for index, element in enumerate(tempListHard):
+            if tempListHard[index] == "empty":
+                tempListHard[index] = ""
+
+    for index, element in enumerate(tempListSoft, start=1):
+        if index != 10:
+            macrosSoft[str(index)] = element
+        else:
+            macrosSoft["0"] = element
+
+    for index, element in enumerate(tempListHard, start=1):
+        if index != 10:
+            macrosHard[f"{str(index)}"] = element
+        else:
+            macrosHard["0"] = element
+
+    while True:
+        if keyboard.is_pressed(softModifier):
+            chosenDict = macrosSoft
+            keyboard.on_release(macroMatch)
+        if keyboard.is_pressed(hardModifier):
+            chosenDict = macrosHard
+            keyboard.on_release(macroMatch)
+        time.sleep(0.0001)
